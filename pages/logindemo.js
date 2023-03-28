7a7081e87510a29f9a0159f4da0764576c218593
harpreet1602 <harpreets1602@gmail.com>
2023-03-28 14:08:55 +0530

    Main pages of information completed

---
+++ pages/logindemo.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import logindemo from './public/logindemo.jpg';
+
+const Logindemo = () => {
+    return (
+        <>
+        {/* // <img src={logindemo} className="flex justify-center" /> */}
+        <div className="flex justify-center text-4xl font-extrabold">Login page coming soon...</div>
+        </>  
+    )
+}
+
+export default Logindemo;
